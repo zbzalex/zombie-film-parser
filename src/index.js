d48b4627b43381ce78970adddf0cf7597a971003
macbook15 <macbook15@MacBook-Pro-macbook15.local>
2023-06-06 20:01:00 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -6,18 +6,9 @@ const axios = require('axios');
 // https://api.zombie-film.live/v2/franchise/view/?slug=goryachaya-zona&season=2&findBy=details&ref=https:%2F%2Fzombie-film.live&_format=json
 
 function parseKinogramScript(source) {
-  if (/var app = makePlayer\(/i.exec(source) !== null) {
-    const result = eval(`(function() {
-      function makePlayer(options) {
-        return options;
-      }
-
-      ${source}
-      ;
-
-      return app;
-    })
-    `)();
+  source = source.replace(/<script[^>]+>|<\/script>/g, '')
+  if (/makePlayer\(/i.exec(source) !== null) {
+    const result = eval(`function makePlayer(options) { return options; } ${source}`);
 
     return result.playlist.seasons;
   }
@@ -27,18 +18,19 @@ function parseKinogramScript(source) {
 
 axios.get('https://api.kinogram.best/embed/movie/353').then((response) => {
   const body = response.data;
-  const re = /<script>(.*?)<\/script>/gmsu;
+  const re = /<script data-name="mk">(.+)<\/script>/gmsu;
   let matches;
   while ((matches = re.exec(body)) !== null) {
     const script = matches[1];
-
     const result = parseKinogramScript(script);
     if (result !== null) {
-      // result.forEach((item) => {
-      //   item.episodes.forEach(episode => {
-      //     console.log(episode.hls)
-      //   })
-      // });
+      result.forEach((item) => {
+        console.log(item.season)
+        item.episodes.forEach(episode => {
+          console.log(episode.hls)
+          console.log(episode.episode)
+        })
+      });
 
       break;
     }
